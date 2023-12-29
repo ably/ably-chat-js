d6851871ab7d94997a6072dd43a548feaff9de52
evgeny <khokhlov.e.n@gmail.com>
2023-12-29 16:51:00 +0000

    feat: update demo

--- demo/src/components/Message/Message.tsx
+++ demo/src/components/Message/Message.tsx
@@ -1,29 +1,36 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useCallback } from 'react';
 import clsx from 'clsx';
 
 interface MessageProps {
+  id: string;
   self?: boolean;
   children?: ReactNode | undefined;
+  onMessageClick?(id: string): void;
 }
-export const Message: React.FC<MessageProps> = ({ self = false, children }) => {
+export const Message: React.FC<MessageProps> = ({ id, self = false, children, onMessageClick }) => {
+  const handleMessageClick = useCallback(() => {
+    onMessageClick?.(id);
+  }, [id, onMessageClick]);
+
   return (
-    <div className="chat-message">
+    <div
+      className="chat-message"
+      onClick={handleMessageClick}
+    >
       <div className={clsx('flex items-end', { ['justify-end']: self, ['justify-start']: !self })}>
         <div
-          className={clsx('flex flex-col space-y-2 text-xs max-w-xs mx-2', {
+          className={clsx('flex flex-col space-y-2 text max-w-xs mx-2', {
             ['items-end order-1']: self,
             ['items-start order-2']: !self,
           })}
         >
-          <div>
-            <span
-              className={clsx('px-4 py-2 rounded-lg inline-block', {
-                ['rounded-br bg-blue-600 text-white']: self,
-                ['rounded-bl justify-start bg-gray-300 text-gray-600']: !self,
-              })}
-            >
-              {children}
-            </span>
+          <div
+            className={clsx('px-4 py-2 rounded-lg inline-block', {
+              ['rounded-br bg-blue-600 text-white']: self,
+              ['rounded-bl justify-start bg-gray-300 text-gray-600']: !self,
+            })}
+          >
+            {children}
           </div>
         </div>
       </div>
