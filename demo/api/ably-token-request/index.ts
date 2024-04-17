fe6aea009f75b455b8deabfd49d31a2a20a2f976
Andy Ford <andy.ford@ably.com>
2024-04-17 11:57:29 +0100

    fix: rename ably api key env var
    This is to avoid clashes (Vite does not overwrite existing vars) with other environment variables that a user may have set. It removes a source
    of clashes for Ablyians working on it (as ABLY_API_KEY is often used for communicating with the production service).
    
    I would suggest we change this back again in the future once things are stable in production.

--- demo/api/ably-token-request/index.ts
+++ demo/api/ably-token-request/index.ts
@@ -1,28 +1,28 @@
 import * as dotenv from 'dotenv';
-import * as Ably from 'ably'
+import Ably from 'ably'
 import { HandlerEvent } from '@netlify/functions';
 
 dotenv.config();
 
 export async function handler(event: HandlerEvent) {
-  if (!process.env.ABLY_API_KEY) {
+  if (!process.env.VITE_ABLY_CHAT_API_KEY) {
     console.error(`
-Missing ABLY_API_KEY environment variable.
-If you're running locally, please ensure you have a ./.env file with a value for ABLY_API_KEY=your-key.
-If you're running in Netlify, make sure you've configured env variable ABLY_API_KEY.
+Missing VITE_ABLY_CHAT_API_KEY environment variable.
+If you're running locally, please ensure you have a ./.env file with a value for VITE_ABLY_CHAT_API_KEY=your-key.
+If you're running in Netlify, make sure you've configured env variable VITE_ABLY_CHAT_API_KEY.
 
 Please see README.md for more details on configuring your Ably API Key.`);
 
     return {
       statusCode: 500,
       headers: { 'content-type': 'application/json' },
-      body: JSON.stringify('ABLY_API_KEY is not set'),
+      body: JSON.stringify('VITE_ABLY_CHAT_API_KEY is not set'),
     };
   }
 
   const clientId = event.queryStringParameters?.['clientId'] || process.env.DEFAULT_CLIENT_ID || 'NO_CLIENT_ID';
   const client = new Ably.Rest({
-    key: process.env.ABLY_API_KEY,
+    key: process.env.VITE_ABLY_CHAT_API_KEY,
     restHost: process.env.VITE_ABLY_HOST,
     realtimeHost: process.env.VITE_ABLY_HOST,
   });
