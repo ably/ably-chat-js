fcaaaa4501c2a2dcfc4d4d49e7ccee8e69a19632
Vlad Velici <vlad.velici@ably.com>
2024-04-25 15:34:41 +0100

    initial cleanup of SDK, does not include demo app changes, still a work in progress https://ably.atlassian.net/browse/CHA-183

--- src/entities.ts
+++ src/entities.ts
@@ -1,30 +1,7 @@
-export interface Room {
-  id: string;
-  application_id: string;
-  ttl: number | null;
-  created_at: number;
-}
-
 export interface Message {
   id: string;
   created_by: string;
   room_id: string;
   content: string;
-  reactions:
-    | {
-        counts: Record<string, number>;
-        latest: Reaction[];
-        mine: Reaction[];
-      }
-    | undefined;
   created_at: number;
-  edited_at: number | null;
-  deleted_at: number | null;
-}
-
-export interface Reaction {
-  id: string;
-  message_id: string;
-  type: string;
-  created_by: string;
 }
--- src/events.ts
+++ src/events.ts
@@ -1,10 +1,3 @@
 export enum MessageEvents {
   created = 'message.created',
-  edited = 'message.edited',
-  deleted = 'message.deleted',
-}
-
-export enum ReactionEvents {
-  created = 'reaction.created',
-  deleted = 'reaction.deleted',
 }
