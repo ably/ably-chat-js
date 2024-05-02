8d002fc28c6ebaebc095925e3b3e2117958c440c
Vlad Velici <vlad.velici@ably.com>
2024-05-02 14:05:26 +0100

    remove old imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { Chat } from './Chat.js';
-export { MessageEvents, ReactionEvents } from './events.js';
-export type { Message, Reaction, Room } from './entities.js';
+export { MessageEvents } from './events.js';
+export type { Message } from './entities.js';
 export type { Room as RoomController } from './Room.js';
 export type { MessageListener } from './Messages.js';
-export type { ReactionListener } from './MessageReactions.js';
