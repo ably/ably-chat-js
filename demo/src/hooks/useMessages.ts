56c62fab8b8990fb3a9ac451558c3368825193e5
Vlad Velici <vlad.velici@ably.com>
2024-05-16 12:17:12 +0100

    on init messages: temporary remove history call, until we add history api

--- demo/src/hooks/useMessages.ts
+++ demo/src/hooks/useMessages.ts
@@ -2,9 +2,10 @@ import { Message, MessageEvents, type MessageListener } from '@ably-labs/chat';
 import { useCallback, useEffect, useState } from 'react';
 import { useRoom } from './useRoom';
 
-const combineMessages = (previousMessages: Message[], lastMessages: Message[]) => {
-  return [...previousMessages.filter((msg) => lastMessages.every(({ id }) => id !== msg.id)), ...lastMessages];
-};
+// todo: uncomment. used for history query when we add it back
+// const combineMessages = (previousMessages: Message[], lastMessages: Message[]) => {
+//   return [...previousMessages.filter((msg) => lastMessages.every(({ id }) => id !== msg.id)), ...lastMessages];
+// };
 
 export const useMessages = () => {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -19,23 +20,24 @@ export const useMessages = () => {
   );
 
   useEffect(() => {
-    setLoading(true);
+    setLoading(false); // todo: set to true here when we add back history query (see commented initMessages below)
+
     const handleAdd: MessageListener = ({ message }) => {
       setMessages((prevMessage) => [...prevMessage, message]);
     };
-
     room.messages.subscribe(MessageEvents.created, handleAdd);
 
     let mounted = true;
-    const initMessages = async () => {
-      const lastMessages = await room.messages.query({ limit: 100 });
-      if (mounted) {
-        setLoading(false);
-        setMessages((prevMessages) => combineMessages(prevMessages, lastMessages).reverse());
-      }
-    };
     setMessages([]);
-    initMessages();
+    // const initMessages = async () => {
+    //   const lastMessages = await room.messages.query({ limit: 100 });
+    //   if (mounted) {
+    //     // setLoading(false);
+    //     setMessages((prevMessages) => combineMessages(prevMessages, lastMessages).reverse());
+    //   }
+    // };
+    // initMessages();
+
 
     return () => {
       mounted = false;
