d3142ecf2a3b731180c26753db370ead5e3962a4
Andy Ford <andy.ford@ably.com>
2024-05-20 10:29:58 +0100

    dev: bootstrap integration tests
    format and lint

---
+++ src/helper/testSetup.ts
@@ -0,0 +1,20 @@
+import testAppSetup from '../../ably-common/test-resources/test-app-setup.json';
+
+// Setup creates a new app in the sanbox environment and sets the key
+// This is called automatically by vitest before the tests are run.
+const setup = async () => {
+  const response = await fetch('https://sandbox-rest.ably.io/apps', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(testAppSetup.post_apps),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Response not OK (${response.status})`);
+  }
+
+  const testApp = await response.json();
+  process.env.testAblyApiKey = testApp.keys[0].keyStr;
+};
+
+export { setup };
