2901997efe51403880c39041632aa0d2f98643c0
Andy Ford <andy.ford@ably.com>
2024-05-31 08:13:34 +0100

    messages: use managed channel for chat messages
    Use the new subscription manager for chat messages, so we get the implicit
    detach behaviour when unsubscribing from chat messages.

--- src/Room.ts
+++ src/Room.ts
@@ -8,7 +8,6 @@ import { DEFAULT_CHANNEL_OPTIONS } from './version.js';
 export class Room {
   private readonly _roomId: string;
   private readonly chatApi: ChatApi;
-  readonly subscriptionManager: DefaultSubscriptionManager;
   readonly messages: Messages;
   readonly presence: Presence;
   private readonly realtimeChannelName: string;
@@ -17,11 +16,12 @@ export class Room {
     this._roomId = roomId;
     this.chatApi = chatApi;
     this.realtimeChannelName = `${this._roomId}::$chat::$chatMessages`;
-    this.subscriptionManager = new DefaultSubscriptionManager(
+
+    const subscriptionManager = new DefaultSubscriptionManager(
       realtime.channels.get(this.realtimeChannelName, DEFAULT_CHANNEL_OPTIONS),
     );
-    this.messages = new Messages(roomId, realtime, this.chatApi, realtime.auth.clientId);
-    this.presence = new Presence(this.subscriptionManager, realtime.auth.clientId);
+    this.messages = new Messages(roomId, subscriptionManager, this.chatApi, realtime.auth.clientId);
+    this.presence = new Presence(subscriptionManager, realtime.auth.clientId);
   }
 
   get roomId(): string {
