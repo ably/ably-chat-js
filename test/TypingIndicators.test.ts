19b42a13d8e910c3fce317462c46c67f15333d0f
Andy Ford <andy.ford@ably.com>
2024-06-19 12:04:56 +0100

    typing: check valid clientid on event, add tests

--- test/TypingIndicators.test.ts
+++ test/TypingIndicators.test.ts
@@ -1,5 +1,5 @@
 import * as Ably from 'ably';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, test, vi } from 'vitest';
 
 import { ChatClient } from '../src/Chat.js';
 import { Room } from '../src/Room.js';
@@ -11,6 +11,8 @@ interface TestContext {
   chat: ChatClient;
   room: Room;
   roomId: string;
+  emulateBackendPublish: Ably.messageCallback<Ably.PresenceMessage>;
+  channelLevelListeners: Set<Ably.messageCallback<Ably.PresenceMessage>>;
 }
 
 const TEST_TYPING_TIMEOUT_MS = 100;
@@ -22,8 +24,38 @@ describe('TypingIndicators', () => {
     context.realtime = new Ably.Realtime({ clientId: 'clientId', key: 'key' });
     context.roomId = randomRoomId();
     context.chat = newChatClient({ typingTimeoutMs: TEST_TYPING_TIMEOUT_MS }, context.realtime);
-
     context.room = context.chat.rooms.get(context.roomId);
+    context.channelLevelListeners = new Set();
+
+    const channel = context.realtime.channels.get('roomId');
+    const presence = channel.presence;
+
+    vi.spyOn(presence, 'subscribe').mockImplementation(
+      // @ts-ignore
+      async (listener: Ably.messageCallback<Ably.PresenceMessage>) => {
+        context.channelLevelListeners.add(listener);
+
+        // @ts-ignore
+        context.emulateBackendPublish = (msg) => {
+          context.channelLevelListeners.forEach((_, cb) => cb(msg));
+        };
+      },
+    );
+
+    vi.spyOn(presence, 'unsubscribe').mockImplementation(
+      // @ts-ignore
+      async (listener: Ably.messageCallback<Ably.PresenceMessage>) => {
+        context.channelLevelListeners.delete(listener);
+      },
+    );
+
+    // Mock the attach
+    vi.spyOn(channel, 'attach').mockImplementation(async () => {
+      return null;
+    });
+
+    // Mock the detach
+    vi.spyOn(channel, 'detach').mockImplementation(async () => {});
   });
 
   it<TestContext>('delays stopTyping timeout while still typing', async (context) => {
@@ -74,5 +106,44 @@ describe('TypingIndicators', () => {
 
     // Ensure that leaveClient was called only once by the stopTyping method and not again when the timer expires
     expect(presence.leaveClient).toHaveBeenCalledOnce();
+  });
+
+  type PresenceTestParam = Omit<Ably.PresenceMessage, 'action' | 'clientId'>;
+
+  describe.each([
+    ['no client id', { connectionId: '', id: '', encoding: '', timestamp: 0, extras: {}, data: {} }],
+    ['empty client id', { clientId: '', connectionId: '', id: '', encoding: '', timestamp: 0, extras: {}, data: {} }],
+  ])('invalid incoming presence messages: %s', (description: string, inbound: PresenceTestParam) => {
+    const invalidPresenceTest = (context: TestContext, presenceAction: Ably.PresenceAction) =>
+      new Promise<void>((done, reject) => {
+        const { room } = context;
+
+        room.typingIndicators
+          .subscribe(() => {
+            reject(new Error('Should not have received a typing event'));
+          })
+          .then(() => {
+            context.emulateBackendPublish({
+              ...inbound,
+              action: presenceAction,
+            } as Ably.PresenceMessage);
+          })
+          .then(() => {
+            done();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+
+    describe.each([
+      ['enter' as Ably.PresenceAction],
+      ['leave' as Ably.PresenceAction],
+      ['present' as Ably.PresenceAction],
+      ['update' as Ably.PresenceAction],
+    ])(`does not process invalid presence %s message: ${description}`, (presenceAction: Ably.PresenceAction) => {
+      test<TestContext>(`does not process invalid presence ${presenceAction} message: ${description}`, (context) =>
+        invalidPresenceTest(context, presenceAction));
+    });
   });
 });
