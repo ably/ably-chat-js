1e9a78e8c5b6441a3fb18823ebbcc9c64664982f
Andy Ford <andy.ford@ably.com>
2024-06-19 12:26:40 +0100

    auth: add smoke test for chat with basic auth

--- test/Chat.integration.test.ts
+++ test/Chat.integration.test.ts
@@ -3,6 +3,7 @@ import { describe, expect, it } from 'vitest';
 import { RealtimeWithOptions } from '../src/realtimeextensions.js';
 import { newChatClient } from './helper/chat.js';
 import { testClientOptions } from './helper/options.js';
+import { ablyRealtimeClient } from './helper/realtimeClient.js';
 
 describe('Chat', () => {
   it('should set the agent string', () => {
@@ -13,5 +14,18 @@ describe('Chat', () => {
   it('should mix in the client options', () => {
     const chat = newChatClient(testClientOptions({ typingTimeoutMs: 1000 }));
     expect(chat.clientOptions.typingTimeoutMs).toBe(1000);
+  });
+
+  it('should work using basic auth', async () => {
+    const chat = newChatClient(undefined, ablyRealtimeClient({}));
+
+    // Send a message, and expect it to succeed
+    await chat.rooms.get('test').messages.send('my message');
+
+    // Request occupancy, and expect it to succeed
+    await chat.rooms.get('test').occupancy.get();
+
+    // Request history, and expect it to succeed
+    await chat.rooms.get('test').messages.query({ limit: 1 });
   });
 });
