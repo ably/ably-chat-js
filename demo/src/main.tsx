b024aa79e02ba212603eebb09382a9d982a159c9
Vlad Velici <vlad.velici@ably.com>
2024-06-24 14:10:43 +0100

    Make the chat demo app use production Ably by default
    CHA-326

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -20,20 +20,20 @@ const clientId = function(){
 
 // use this for local development with local realtime
 //
-const ablyClient = new Ably.Realtime({
-  authUrl: `/api/ably-token-request?clientId=${clientId}`,
-  port: 8081,
-  environment: 'local',
-  tls: false,
-  clientId,
-});
-
 // const ablyClient = new Ably.Realtime({
 //   authUrl: `/api/ably-token-request?clientId=${clientId}`,
-//   restHost: import.meta.env.VITE_ABLY_HOST,
-//   realtimeHost: import.meta.env.VITE_ABLY_HOST,
+//   port: 8081,
+//   environment: 'local',
+//   tls: false,
 //   clientId,
-// })
+// });
+
+const ablyClient = new Ably.Realtime({
+  authUrl: `/api/ably-token-request?clientId=${clientId}`,
+  restHost: import.meta.env.VITE_ABLY_HOST,
+  realtimeHost: import.meta.env.VITE_ABLY_HOST,
+  clientId,
+})
 
 const chatClient = new ChatClient(ablyClient, {logLevel: LogLevel.debug });
 
