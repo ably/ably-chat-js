8f7896d4eb677aed3a029284529b7c5cf63b0274
Andy Ford <andy.ford@ably.com>
2024-07-18 08:43:28 +0100

    test: add test for config branch

---
+++ test/core/config.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+
+import { normalizeClientOptions } from '../../src/core/config.ts';
+import { LogLevel } from '../../src/core/logger.ts';
+
+describe('config', () => {
+  it('normalizes client options with no options', () => {
+    expect(normalizeClientOptions()).toEqual({
+      logLevel: LogLevel.Error,
+    });
+  });
+
+  it('normalizes client options with logLevel', () => {
+    expect(normalizeClientOptions({ logLevel: LogLevel.Debug })).toEqual({
+      logLevel: LogLevel.Debug,
+    });
+  });
+
+  it('normalizes client options with logHandler', () => {
+    const logHandler = () => {};
+    expect(normalizeClientOptions({ logHandler })).toEqual({
+      logHandler,
+      logLevel: LogLevel.Error,
+    });
+  });
+});
