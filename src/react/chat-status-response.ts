0165fcfe4bf9251a11f113b3c371ce0a971acabd
Vlad Velici <vlad.velici@ably.com>
2024-07-23 16:10:23 +0100

    react hooks base interfaces: add to index and typedoc

--- src/react/chat-status-response.ts
+++ src/react/chat-status-response.ts
@@ -1,8 +1,19 @@
 import { ConnectionStatus, ErrorInfo, RoomStatus } from '@ably/chat';
 
+/**
+ * Common status variables for chat features. Most hooks in this library
+ * implement this interface.
+ */
 export interface ChatStatusResponse {
+  /** Provides access to get or listen to the connection to Ably. */
   readonly connectionStatus: ConnectionStatus;
+
+  /** If there's a connection error it will be available here.  */
   readonly connectionError?: ErrorInfo;
+
+  /** Provides access to get or listen to the room status. */
   readonly roomStatus: RoomStatus;
+
+  /** If there's an error with the room it will be available here. */
   readonly roomError?: ErrorInfo;
 }
--- src/react/listenable.ts
+++ src/react/listenable.ts
@@ -1,3 +1,7 @@
+/**
+ * Hooks that provide events that can be listened to implement this interface
+ * to allow passing a listener when using the hook.
+ */
 export interface Listenable<T> {
   listener?: T;
 }
