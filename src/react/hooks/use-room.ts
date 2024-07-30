7ebd57d116397bc7f4ec5117b4e3edaec8b89eb1
Vlad Velici <vlad.velici@ably.com>
2024-07-30 20:54:48 +0100

    react: add useRoom hook with related context and provider
    Adds RoomProvider, RoomContext and useRoom hook to the react hooks and
    adapts the demo app to use them.
    
    This version uses two parameters on RoomProvider:
    
    - `attach` controls whether RoomProvider attaches the room on mount and
    automatically detaches on unmount. Defaults to true.
    - `release` controls whether the room will be automatically released
    on unmount. Note that releasing a room also detaches it. Defaults to
    true.
    
    The two parameters are useful for cases where the same room must be
    provided more than once and a single component's mount/unmount is not
    suitable to control attach, detach and release behaviors.
    
    We have a known issue with the message channel auto-attaching on
    presence.get, and the typing indicators channel auto-attaching due to
    typing indicators working via presence (but on a different channel).
    
    This issue affects the demo app if RoomProvider is used with release=true
    and attach=false.

---
+++ src/react/hooks/use-room.ts
@@ -0,0 +1,98 @@
+import { ConnectionStatusChange, ErrorInfo, Room, RoomLifecycle, RoomStatusChange } from '@ably/chat';
+import { useContext, useEffect, useState } from 'react';
+
+import { ChatStatusResponse } from '../chat-status-response.js';
+import { RoomContext } from '../contexts/room-context.js';
+import { useChatConnection } from './use-chat-connection.js';
+
+/**
+ * Parameters for the useRoom hook.
+ */
+export interface UseRoomParams {
+  /**
+   * Callback for when the room status changes. The listener is removed when
+   * the component unmounts.
+   * @param change The change object.
+   */
+  onStatusChange?: (change: RoomStatusChange) => void;
+
+  /**
+   * Callback for when the connection status changes. The listener is removed
+   * when the component unmounts.
+   * @param change The change object.
+   */
+  onConnectionStatusChange?: (change: ConnectionStatusChange) => void;
+}
+
+/**
+ * The return type for the useRoom hook.
+ */
+export interface UseRoomResponse extends ChatStatusResponse {
+  /** The room object. */
+  room: Room;
+
+  /**
+   * Shortcut to room.attach. Not needed if room provider has attach=true set,
+   * which is the default behavior.
+   */
+  attach: () => Promise<void>;
+
+  /**
+   * Shortcut to room.detach. Not needed if room provider has attach=true
+   * set, which is the default behavior.
+   */
+  detach: () => Promise<void>;
+}
+
+/**
+ * Hook that provides access to the current room.
+ *
+ * @param params Register optional callbacks, see {@link UseRoomParams}.
+ * @returns {@link UseRoomResponse}
+ */
+export const useRoom = (params?: UseRoomParams): UseRoomResponse => {
+  const context = useContext(RoomContext);
+
+  if (!context) throw new Error('useRoom: RoomContext not found.');
+
+  const { currentStatus: connectionStatus, error: connectionError } = useChatConnection({
+    onStatusChange: params?.onConnectionStatusChange,
+  });
+
+  const room = context.room;
+
+  // room error and status callbacks
+  const [roomStatus, setRoomStatus] = useState<{
+    status: RoomLifecycle;
+    error?: ErrorInfo;
+  }>({
+    status: room.status.current,
+    error: room.status.error,
+  });
+
+  const onRoomStatusChange = params?.onStatusChange;
+  useEffect(() => {
+    const { off } = room.status.onChange((change) => {
+      setRoomStatus({
+        status: change.current,
+        error: change.error,
+      });
+      if (onRoomStatusChange) {
+        onRoomStatusChange(change);
+      }
+    });
+    return () => {
+      off();
+    };
+  }, [room, onRoomStatusChange]);
+
+  return {
+    room: room,
+    attach: room.attach.bind(room),
+    detach: room.detach.bind(room),
+    roomStatus: roomStatus.status,
+    roomError: roomStatus.error,
+    connectionStatus: connectionStatus,
+    connectionError: connectionError,
+  };
+};
