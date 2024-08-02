d49df3bcf36c10f697f34e8f103db355cfff58d2
Vlad Velici <vlad.velici@ably.com>
2024-08-02 13:30:51 +0100

    demo: make connection status bar fixed on top so it won't make content jump

--- demo/src/components/ConnectionStatusComponent/ConnectionStatusComponent.tsx
+++ demo/src/components/ConnectionStatusComponent/ConnectionStatusComponent.tsx
@@ -7,18 +7,44 @@ export const ConnectionStatusComponent: React.FC = () => {
 
   switch (currentStatus) {
     case ConnectionLifecycle.Connecting: {
-      return <div className="text-blue-800 bg-blue-50">Connecting...</div>;
+      return (
+        <div
+          className="text-blue-800 bg-blue-50"
+          style={{ position: 'fixed', top: '0', padding: '3px' }}
+        >
+          Connecting...
+        </div>
+      );
     }
     case ConnectionLifecycle.Disconnected: {
-      return <div className="text-yellow-800 bg-yellow-50">Disconnected - will retry to connect automatically</div>;
+      return (
+        <div
+          className="text-yellow-800 bg-yellow-50"
+          style={{ position: 'fixed', top: '0', padding: '3px' }}
+        >
+          Disconnected - will retry to connect automatically
+        </div>
+      );
     }
     case ConnectionLifecycle.Suspended: {
       return (
-        <div className="text-yellow-800 bg-yellow-50">Connection suspended - will retry to connect automatically</div>
+        <div
+          className="text-yellow-800 bg-yellow-50"
+          style={{ position: 'fixed', top: '0', padding: '3px' }}
+        >
+          Connection suspended - will retry to connect automatically
+        </div>
       );
     }
     case ConnectionLifecycle.Failed: {
-      return <div className="text-red-800 bg-red-50">Connection failed. Refresh the page to try again.</div>;
+      return (
+        <div
+          className="text-red-800 bg-red-50"
+          style={{ position: 'fixed', top: '0', padding: '3px' }}
+        >
+          Connection failed. Refresh the page to try again.
+        </div>
+      );
     }
   }
 
