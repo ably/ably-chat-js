240cf7729ba9f5d1c9df6f055df54be1e36c4812
Andy Ford <andy.ford@ably.com>
2024-09-18 15:45:01 +0100

    release: bump version to 0.2.1

--- src/core/version.ts
+++ src/core/version.ts
@@ -1,4 +1,4 @@
 // Update this when you release a new version
-export const VERSION = '0.2.0';
+export const VERSION = '0.2.1';
 export const CHANNEL_OPTIONS_AGENT_STRING = `chat-js/${VERSION}`;
 export const DEFAULT_CHANNEL_OPTIONS = { params: { agent: CHANNEL_OPTIONS_AGENT_STRING } };
