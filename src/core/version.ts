a9de20b65a6791fec370becd522b2c0088b0baa4
Andy Ford <andy.ford@ably.com>
2025-06-18 08:46:56 +0100

    refactor: re-add ChannelOptions type for clarity

--- src/core/version.ts
+++ src/core/version.ts
@@ -6,13 +6,13 @@ export const CHANNEL_OPTIONS_AGENT_STRING = `chat-js/${VERSION}`;
 export const CHANNEL_OPTIONS_AGENT_STRING_REACT = `chat-react/${VERSION}`;
 // Modes required for basic message functionality
 export const DEFAULT_CHANNEL_MODES: Ably.ChannelMode[] = ['PUBLISH', 'SUBSCRIBE'];
-export const DEFAULT_CHANNEL_OPTIONS = {
+export const DEFAULT_CHANNEL_OPTIONS: Ably.ChannelOptions = {
   params: { agent: CHANNEL_OPTIONS_AGENT_STRING },
   attachOnSubscribe: false,
   modes: DEFAULT_CHANNEL_MODES,
 };
 
-export const DEFAULT_CHANNEL_OPTIONS_REACT = {
+export const DEFAULT_CHANNEL_OPTIONS_REACT: Ably.ChannelOptions = {
   params: { agent: CHANNEL_OPTIONS_AGENT_STRING_REACT },
   attachOnSubscribe: false,
   modes: DEFAULT_CHANNEL_MODES,
