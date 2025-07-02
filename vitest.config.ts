a73bae3ee7e2f472aaebd5625b47f5e40eb2b4fa
Andy Ford <andy.ford@ably.com>
2025-07-02 09:54:27 +0100

    refactor: consolidate Vitest configuration into vitest.config.ts and remove vitest.workspace.ts
    The workspace file has been deprecated and will shortly be removed.

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,5 +17,27 @@ export default defineConfig({
       provider: 'v8',
       ignoreEmptyLines: true,
     },
+    projects: [
+      {
+        root: '.',
+        test: {
+          globalSetup: './test/helper/test-setup.ts',
+          setupFiles: ['./test/helper/expectations.ts'],
+          include: ['test/core/**/*.test.{ts,js}'],
+          name: 'chat',
+          environment: 'node',
+        },
+      },
+      {
+        root: '.',
+        test: {
+          globalSetup: './test/helper/test-setup.ts',
+          setupFiles: ['./test/helper/expectations.ts'],
+          include: ['test/react/**/*.test.{tsx,jsx,ts}'],
+          name: 'react-hooks',
+          environment: 'jsdom',
+        },
+      },
+    ],
   },
 });
