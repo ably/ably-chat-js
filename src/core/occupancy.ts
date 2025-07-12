81f2cbd2690a3fa02431e202aed8343f254584c7
Andy Ford <andy.ford@ably.com>
2025-07-12 13:49:51 +0100

    refactor: add helper function for eventEmitter listener counts

--- src/core/occupancy.ts
+++ src/core/occupancy.ts
@@ -6,7 +6,7 @@ import { OccupancyEvent, OccupancyEventType, RealtimeMetaEventType } from './eve
 import { Logger } from './logger.js';
 import { InternalRoomOptions } from './room-options.js';
 import { Subscription } from './subscription.js';
-import EventEmitter, { wrap } from './utils/event-emitter.js';
+import EventEmitter, { emitterHasListeners, wrap } from './utils/event-emitter.js';
 
 /**
  * This interface is used to interact with occupancy in a chat room: subscribing to occupancy updates and
@@ -278,7 +278,6 @@ export class DefaultOccupancy implements Occupancy {
    * @returns true if there are listeners, false otherwise.
    */
   hasListeners(): boolean {
-    const numListeners = this._emitter.listeners()?.length;
-    return numListeners ? numListeners > 0 : false;
+    return emitterHasListeners(this._emitter);
   }
 }
